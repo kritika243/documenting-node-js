9f02d5c6234f26f7dd934328a205081e555a49bc
kritika243 <24kritikasharma24@gmail.com>
2021-09-07 13:51:18 +0530

    routing

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
 
+  let path = './views/'
+
+  switch (req.url) {
+    case '/':
+      path += 'index.html'
+      break
+
+    case '/about':
+      path += 'about.html'
+      break
+
+    default:
+      path += '404.html'
+      break
+  }
+
   /* ============ Sending a Response - 3 steps ============= */
   // set header content type
   res.setHeader('Content-Type', 'text/html')
 
   // send a html file
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err)
       res.end()
