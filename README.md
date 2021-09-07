61e6e630e64c5b616f9817555d6649e3d16153f6
kritika243 <24kritikasharma24@gmail.com>
2021-09-07 13:17:12 +0530

    request and response

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - Setting up a server
 - Localhost: it is like a domain name on the web
 - Port Number: are like doors into a computer
+- Request and Response Object
+- Listen for a request
+- Send a reponse
+  - set Content-Type eg text/plain or html
+  - write the response
+  - end and then send the response
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log('request made')
+  console.log(req.url, req.method)
+
+  /* ============ Sending a Response - 3 steps ============= */
+  // set header content type
+  res.setHeader('Content-Type', 'text/plain')
+  // write the response
+  res.write('hello world, this is the response')
+  // end the response, which then sends it to the browser
+  res.end()
 })
 
 server.listen(3000, 'localhost', () => {
