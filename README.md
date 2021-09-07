5edaaba885a5170cfa6f591ed0b823f8724f51c1
kritika243 <24kritikasharma24@gmail.com>
2021-09-07 23:01:30 +0530

    using ejs, dynamically update content

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 - View Engines (for rendering view to the browser)
 - ejs
+- EJS templates are processes through the EJS view engine on the server
 - using ejs to render pages
+- Passing data into views
+  - using JavaScript inside views, dynamicaally passing content
+  - use of <% %>
+  - use of <%= %>
--- app.js
+++ app.js
@@ -9,19 +9,33 @@ app.set('view engine', 'ejs')
 app.listen(3000)
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const blogs = [
+    {
+      title: 'Saleb likes PHP',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Aron prefers GoLang',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Jacob: How to defeat browser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+  ]
+  res.render('index', { title: 'Home', blogs: blogs })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about', { title: 'About' })
 })
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create')
+  res.render('create', { title: 'New Blog' })
 })
 
 // 404 page
 app.use((req, res) => {
   // we cam chain like this because res.status() also retuens res object
-  res.status(404).render('404')
+  res.status(404).render('404', { title: '404' })
 })
