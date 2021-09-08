441a04d3985e54a902e875e8fe2796983aab6af9
kritika243 <24kritikasharma24@gmail.com>
2021-09-08 13:09:04 +0530

    :heavy_plus_sign: morgan- logger middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const expres = require('express')
+const morgan = require('morgan')
 
 const app = expres()
 
@@ -7,14 +8,6 @@ app.set('view engine', 'ejs')
 
 // listen for request
 app.listen(3000)
-
-app.use((req, res, next) => {
-  console.log('new request made')
-  console.log('host: ', req.hostname)
-  console.log('path: ', req.path)
-  console.log('method: ', req.method)
-  next()
-})
 
 app.get('/', (req, res) => {
   const blogs = [
