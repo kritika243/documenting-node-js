ac8820659f27ae9a6208d2d13dd9b94ec8173ee1
kritika243 <24kritikasharma24@gmail.com>
2021-09-10 23:46:45 +0530

    r:recycle: refactoring further by separating into controllers

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,52 +1,12 @@
 const express = require('express')
-const Blog = require('../models/blog')
+const blogController = require('../controllers/blogController')
 const router = express.Router()
 
 // blog routes
-router.get('/create', (req, res) => {
-  res.render('create', { title: 'New Blog' })
-})
-
-router.get('/', (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result })
-    })
-    .catch((err) => console.log(err))
-})
-
-router.post('/', (req, res) => {
-  // passing the form data to the Blog document
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect('/blogs')
-    })
-    .catch((err) => console.log(err))
-})
-
-router.get('/:id', (req, res) => {
-  // to first extract the id
-  const id = req.params.id
-  // retrieve the blog from the db using the id
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { blog: result, title: 'Read All' })
-    })
-    .catch((err) => console.log(err))
-})
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: '/blogs' })
-    })
-    .catch((err) => console.log(err))
-})
+router.get('/create', blogController.blog_create_get)
+router.get('/', blogController.blog_index)
+router.post('/', blogController.blog_create_post)
+router.get('/:id', blogController.blog_details)
+router.delete('/:id', blogController.blog_delete)
 
 module.exports = router
