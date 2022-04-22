38ba8ea0e1135cb551251318f1154e64368e6305
kritika243 <24kritikasharma24@gmail.com>
2022-04-22 13:12:14 +0530

    🎉 starting off with practicing basics

---
+++ introduction/intro.js
@@ -0,0 +1,8 @@
+// the console is in the terminal
+console.log('Hello World')
+
+// global object instead of window object
+console.log(global)
+
+// has common core modules
+// CommonJs modules instead of ES6 imports - 'require' instead of import
