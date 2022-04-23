8489e33c3b29ab539db04d0a9df227ff79c22c99
kritika243 <24kritikasharma24@gmail.com>
2022-04-23 21:47:02 +0530

    os core modules

--- introduction/intro.js
+++ introduction/intro.js
@@ -2,7 +2,11 @@
 console.log('Hello World')
 
 // global object instead of window object
-console.log(global)
+// console.log(global)
 
 // has common core modules
 // CommonJs modules instead of ES6 imports - 'require' instead of import
+const os = require('os')
+console.log(os.type())
+console.log(os.homedir())
+console.log(os.version())
