0d30fd38075b557334710e80356fa09a161a001f
kritika243 <24kritikasharma24@gmail.com>
2022-04-24 16:37:48 +0530

    custom module exorts and imports

--- introduction/intro.js
+++ introduction/intro.js
@@ -6,18 +6,22 @@ console.log('Hello World')
 
 // has common core modules
 // CommonJs modules instead of ES6 imports - 'require' instead of import
-const os = require('os')
-const path = require('path')
+// const os = require('os')
+// const path = require('path')
 
-console.log(os.type())
-console.log(os.homedir())
-console.log(os.version())
+// console.log(os.type())
+// console.log(os.homedir())
+// console.log(os.version())
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
 
-console.log(path.parse(__filename))
+// console.log(path.parse(__filename))
+
+const math = require('./math')
+console.log(math.add(2, 6))
+console.log(math.divide(45, 5))
