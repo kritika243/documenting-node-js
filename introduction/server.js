d17812037b643df11c13e893e18e88edaeafed45
kritika243 <24kritikasharma24@gmail.com>
2022-04-25 13:31:54 +0530

    🚚 relocating files and creating and listening to a server using http module

---
+++ introduction/server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log(req)
+  res.end('Hello from the server')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('listening to request on port 8000')
+})
