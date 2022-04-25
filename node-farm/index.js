354db5e87b31c004965731aa863e291075f46536
kritika243 <24kritikasharma24@gmail.com>
2022-04-25 12:41:32 +0530

    writin to a file using fs module - writeFileSync

--- node-farm/index.js
+++ node-farm/index.js
@@ -3,3 +3,6 @@ const fs = require('fs')
 const text = fs.readFileSync('./txt/input.txt', 'utf-8')
 
 console.log(text)
+
+const text2 = `This is additional new text, ${text}`
+fs.writeFileSync('./txt/output.txt', text2)
