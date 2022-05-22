6973273d34d52f8d9c401496c1f6612ff79333e0
kritika243 <24kritikasharma24@gmail.com>
2022-05-22 18:08:02 +0530

    async await with promises

--- async-patterns/promises/index.js
+++ async-patterns/promises/index.js
@@ -9,6 +9,19 @@ const getData = (path) => {
   })
 }
 
-getData('../dummyTextFiles/first.txt')
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err))
+// getData('../dummyTextFiles/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err))
+
+const start = async () => {
+  try {
+    const first = await getData('../dummyTextFiles/first.txt')
+    const second = await getData('../dummyTextFiles/second.txt')
+    console.log(first)
+    console.log(second)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
