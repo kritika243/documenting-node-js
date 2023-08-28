0d4f9d27fe3a5197088580e78377f7eaf801424e
kritika243 <24kritikasharma24@gmail.com>
2023-08-28 13:14:20 +0530

    events with http module

--- node-basics/events/app.js
+++ node-basics/events/app.js
@@ -1,13 +1,23 @@
-
+const http = require('http')
 const EventEmitter = require('events')
 
-const customEmitter = new EventEmitter()
+// const customEmitter = new EventEmitter()
 
-customEmitter.on('response', (name) => {
-  console.log(`data received, hi ${name}`)
-})
-customEmitter.on('response', () => {
-  console.log(`some other logic`)
+// customEmitter.on('response', (name) => {
+//   console.log(`data received, hi ${name}`)
+// })
+// customEmitter.on('response', () => {
+//   console.log(`some other logic`)
+// })
+
+// customEmitter.emit('response', 'john')
+
+
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+  res.end('Welcome')
 })
 
-customEmitter.emit('response', 'john')
+
+server.listen(5001)
