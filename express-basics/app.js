5ad7093462836870e26499a3b03592306ec93086
kritika243 <24kritikasharma24@gmail.com>
2023-09-03 16:10:19 +0530

    setting up server with http

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,1 +1,11 @@
-console.log('Express Basics')
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+  console.log('user hit the server');
+  res.end('home page')
+})
+
+
+
+server.listen(5001)
