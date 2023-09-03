cd41d4cfd43984c2438e3f5e835527179506e833
kritika243 <24kritikasharma24@gmail.com>
2023-09-03 16:18:17 +0530

    http headers

--- express-basics/app.js
+++ express-basics/app.js
@@ -2,8 +2,9 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-  console.log('user hit the server');
-  res.end('home page')
+  res.writeHead(200, { "content-type": 'text/html' })
+  res.write('<h1>Home page</h1>')
+  res.end()
 })
 
 
