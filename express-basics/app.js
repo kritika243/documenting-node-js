3af1e821c34e4c3c94aba8215eb364e5f7bb8232
kritika243 <24kritikasharma24@gmail.com>
2023-09-10 14:46:55 +0530

    sending json in res

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,21 +1,13 @@
 const express = require('express')
-const path = require('path')
+const { products } = require('./data')
+
 const app = express()
 
-
-// set up static and middleware
-app.use(express.static('./public'))
-
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// })
-
-app.all('*', (req, res) => {
-  res.status(404).send('Resource not found')
+app.get('/', (req, res) => {
+  res.json(products)
 })
 
 
 app.listen(5001, () => {
-  console.log('listening on port 5001')
+  console.log('server on 5001')
 })
