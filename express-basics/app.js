3fe4223342e5f7d05d966e1cc76a4106accf34c2
kritika243 <24kritikasharma24@gmail.com>
2023-09-10 19:03:18 +0530

    params and query string params working

--- express-basics/app.js
+++ express-basics/app.js
@@ -25,6 +25,30 @@ app.get('/api/products/:productID', (req, res) => {
 })
 
 
+// query string params
+app.get('/api/v1/query', (req, res) => {
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((p) => {
+      return p.name.startsWith(search)
+    })
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({
+      message: 'No products match the search',
+      data: []
+    })
+  }
+  res.status(200).json(sortedProducts)
+})
+
+
 app.listen(5001, () => {
   console.log('server on 5001')
 })
