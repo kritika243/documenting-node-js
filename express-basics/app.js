c757057a68d1b3f00d8274e755d210f7c2619824
kritika243 <24kritikasharma24@gmail.com>
2023-09-10 20:38:40 +0530

    middleware basic flow setup

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,52 +1,23 @@
 const express = require('express')
-const { products } = require('./data')
-
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home page </h1> <a href="/api/products" > Products </a> ')
+
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
+
+app.get('/', logger, (req, res) => {
+  res.send('home')
+})
+app.get('/about', logger, (req, res) => {
+
+  res.send('about')
 })
 
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((p) => {
-    const { id, name, image } = p
-    return { id, name, image }
-  })
-  res.json(newProducts)
-})
-
-// params
-app.get('/api/products/:productID', (req, res) => {
-  const singleProduct = products.find((p) => p.id === Number(req.params.productID))
-  if (!singleProduct) {
-    return res.status(404).send('Product not found')
-  }
-  return res.json(singleProduct)
-})
-
-
-// query string params
-app.get('/api/v1/query', (req, res) => {
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((p) => {
-      return p.name.startsWith(search)
-    })
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({
-      message: 'No products match the search',
-      data: []
-    })
-  }
-  res.status(200).json(sortedProducts)
-})
 
 
 app.listen(5001, () => {
