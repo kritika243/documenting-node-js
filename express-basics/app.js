9d725a02ca37e375a542935ee13df8cbc3211b4b
kritika243 <24kritikasharma24@gmail.com>
2023-09-10 15:40:36 +0530

    route params

--- express-basics/app.js
+++ express-basics/app.js
@@ -4,7 +4,24 @@ const { products } = require('./data')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json(products)
+  res.send('<h1>Home page </h1> <a href="/api/products" > Products </a> ')
+})
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((p) => {
+    const { id, name, image } = p
+    return { id, name, image }
+  })
+  res.json(newProducts)
+})
+
+// params
+app.get('/api/products/:productID', (req, res) => {
+  const singleProduct = products.find((p) => p.id === Number(req.params.productID))
+  if (!singleProduct) {
+    return res.status(404).send('Product not found')
+  }
+  return res.json(singleProduct)
 })
 
 
