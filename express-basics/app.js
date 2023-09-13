452a48d76c47ccfe56aa181e9f5fb5899c790a58
kritika243 <24kritikasharma24@gmail.com>
2023-09-13 14:13:31 +0530

    using moragn, third party middleware

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const logger = require('./logger')
 const authorize = require('./authorize')
 const app = express()
 
 
-app.use([logger, authorize])
+// app.use([logger, authorize])
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('home')
