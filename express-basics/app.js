ef031cfb2d64ec0a68be4931da4fe752edb30da7
kritika243 <24kritikasharma24@gmail.com>
2023-09-15 13:32:17 +0530

    put method

--- express-basics/app.js
+++ express-basics/app.js
@@ -23,6 +23,23 @@ app.post('/api/people', (req, res) => {
   res.status(201).json({ success: true, person: name })
 })
 
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  const person = people.find((p) => p.id === Number(id))
+  if (!person) {
+    return res.status(404).json({ success: false, msg: 'No person with id' })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
+
 app.post('/login', (req, res) => {
   const { name } = req.body
   if (name) {
