8d108f0675b1baf36361d7e2a0d65add6eba8553
kritika243 <24kritikasharma24@gmail.com>
2023-09-15 14:01:15 +0530

    express router

---
+++ express-basics/routes/people.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+let { people } = require('../data')
+
+
+router.get('/', (req, res) => {
+  res.status(200).json({ success: true, data: people })
+})
+
+router.post('/', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'Name is required' })
+  }
+  res.status(201).json({ success: true, person: name })
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  const person = people.find((p) => p.id === Number(id))
+  if (!person) {
+    return res.status(404).json({ success: false, msg: 'No person with id' })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
+
+
+module.exports = router
